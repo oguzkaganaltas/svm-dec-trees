ba9450a3f4e0b151ebb1d079ce0b3476f9b027a1
oguz <oguzkaganaltas@gmail.com>
2021-12-30 10:59:33 +0200

    initialize project with given folders

---
+++ dt.py
@@ -0,0 +1,74 @@
+def entropy(bucket):
+    """
+    Calculates the entropy.
+    :param bucket: A list of size num_classes. bucket[i] is the number of
+    examples that belong to class i.
+    :return: A float. Calculated entropy.
+    """
+
+
+def info_gain(parent_bucket, left_bucket, right_bucket):
+    """
+    Calculates the information gain. A bucket is a list of size num_classes.
+    bucket[i] is the number of examples that belong to class i.
+    :param parent_bucket: Bucket belonging to the parent node. It contains the
+    number of examples that belong to each class before the split.
+    :param left_bucket: Bucket belonging to the left child after the split.
+    :param right_bucket: Bucket belonging to the right child after the split.
+    :return: A float. Calculated information gain.
+    """
+
+
+def gini(bucket):
+    """
+    Calculates the gini index.
+    :param bucket: A list of size num_classes. bucket[i] is the number of
+    examples that belong to class i.
+    :return: A float. Calculated gini index.
+    """
+
+
+def avg_gini_index(left_bucket, right_bucket):
+    """
+    Calculates the average gini index. A bucket is a list of size num_classes.
+    bucket[i] is the number of examples that belong to class i.
+    :param left_bucket: Bucket belonging to the left child after the split.
+    :param right_bucket: Bucket belonging to the right child after the split.
+    :return: A float. Calculated average gini index.
+    """
+
+
+def calculate_split_values(data, labels, num_classes, attr_index, heuristic_name):
+    """
+    For every possible values to split the data for the attribute indexed by
+    attribute_index, it divides the data into buckets and calculates the values
+    returned by the heuristic function named heuristic_name. The split values
+    should be the average of the closest 2 values. For example, if the data has
+    2.1 and 2.2 in it consecutively for the values of attribute index by attr_index,
+    then one of the split values should be 2.15.
+    :param data: An (N, M) shaped numpy array. N is the number of examples in the
+    current node. M is the dimensionality of the data. It contains the values for
+    every attribute for every example.
+    :param labels: An (N, ) shaped numpy array. It contains the class values in
+    it. For every value, 0 <= value < num_classes.
+    :param num_classes: An integer. The number of classes in the dataset.
+    :param attr_index: An integer. The index of the attribute that is going to
+    be used for the splitting operation. This integer indexs the second dimension
+    of the data numpy array.
+    :param heuristic_name: The name of the heuristic function. It should either be
+    'info_gain' of 'avg_gini_index' for this homework.
+    :return: An (L, 2) shaped numpy array. L is the number of split values. The
+    first column is the split values and the second column contains the calculated
+    heuristic values for their splits.
+    """
+
+
+def chi_squared_test(left_bucket, right_bucket):
+    """
+    Calculates chi squared value and degree of freedom between the selected attribute
+    and the class attribute. A bucket is a list of size num_classes. bucket[i] is the
+    number of examples that belong to class i.
+    :param left_bucket: Bucket belonging to the left child after the split.
+    :param right_bucket: Bucket belonging to the right child after the split.
+    :return: A float and and integer. Chi squared value and degree of freedom.
+    """
