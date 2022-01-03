79fc10f584f13c3600a7e5d032a9401010300ba1
oguz <oguzkaganaltas@gmail.com>
2022-01-03 23:27:55 +0200

    finished

--- dt.py
+++ dt.py
@@ -1,3 +1,7 @@
+import numpy as np
+from numpy.core.fromnumeric import mean
+
+
 def entropy(bucket):
     """
     Calculates the entropy.
@@ -5,6 +9,16 @@ def entropy(bucket):
     examples that belong to class i.
     :return: A float. Calculated entropy.
     """
+    bucket = np.array(bucket)
+    divider = bucket.sum()
+
+    def entrophy_funct(p):
+        if(p):
+            return -p/divider*np.log2(p/divider)
+        else:
+            return 0.0
+    v_entrophy = np.vectorize(entrophy_funct)
+    return v_entrophy(bucket).sum()
 
 
 def info_gain(parent_bucket, left_bucket, right_bucket):
@@ -17,6 +31,15 @@ def info_gain(parent_bucket, left_bucket, right_bucket):
     :param right_bucket: Bucket belonging to the right child after the split.
     :return: A float. Calculated information gain.
     """
+    parent_bucket = np.array(parent_bucket)
+    left_bucket = np.array(left_bucket)
+    right_bucket = np.array(right_bucket)
+    parent_entrophy = entropy(parent_bucket)
+
+    left = np.sum(left_bucket)/np.sum(parent_bucket) * entropy(left_bucket)
+    right = np.sum(right_bucket)/np.sum(parent_bucket) * entropy(right_bucket)
+
+    return parent_entrophy - (left + right)
 
 
 def gini(bucket):
@@ -26,6 +49,13 @@ def gini(bucket):
     examples that belong to class i.
     :return: A float. Calculated gini index.
     """
+    bucket = np.array(bucket)
+    divider = bucket.sum()
+
+    def calculate_gini(p):
+        return -np.square(p/divider)
+    v_gini = np.vectorize(calculate_gini)
+    return v_gini(bucket).sum() + 1
 
 
 def avg_gini_index(left_bucket, right_bucket):
@@ -36,6 +66,13 @@ def avg_gini_index(left_bucket, right_bucket):
     :param right_bucket: Bucket belonging to the right child after the split.
     :return: A float. Calculated average gini index.
     """
+    left_gini = gini(left_bucket)
+    right_gini = gini(right_bucket)
+
+    left_divider = np.sum(left_bucket)
+    right_divider = np.sum(right_bucket)
+
+    return left_divider/(left_divider+right_divider) * left_gini + right_divider / (left_divider + right_divider) * right_gini
 
 
 def calculate_split_values(data, labels, num_classes, attr_index, heuristic_name):
@@ -61,6 +98,47 @@ def calculate_split_values(data, labels, num_classes, attr_index, heuristic_name
     first column is the split values and the second column contains the calculated
     heuristic values for their splits.
     """
+    def create_bucket(labels, index):
+        left_index, left_count = np.unique(labels[:index], return_counts=True)
+        left_bucket = np.zeros(num_classes)
+        left_bucket[left_index] = left_count
+
+        right_index, right_count = np.unique(
+            labels[index:], return_counts=True)
+        right_bucket = np.zeros(num_classes)
+        right_bucket[right_index] = right_count
+
+        return avg_gini_index(left_bucket, right_bucket)
+
+    def create_info_gain(labels, index):
+        left_index, left_count = np.unique(labels[:index], return_counts=True)
+        left_bucket = np.zeros(num_classes)
+        left_bucket[left_index] = left_count
+
+        right_index, right_count = np.unique(
+            labels[index:], return_counts=True)
+        right_bucket = np.zeros(num_classes)
+        right_bucket[right_index] = right_count
+
+        parent_index, parent_count = np.unique(labels, return_counts=True)
+        parent_bucket = np.zeros(num_classes)
+        parent_bucket[parent_index] = parent_count
+
+        return info_gain(parent_bucket, left_bucket, right_bucket)
+
+    def ginis(labels):
+        if(heuristic_name == "avg_gini_index"):
+            return [create_bucket(labels, i+1) for i in range(len(labels)-1)]
+        else:
+            return [create_info_gain(labels, i+1) for i in range(len(labels)-1)]
+
+    sorted_indexes = np.argsort(data[:, attr_index])
+    sorted_column = data[:, attr_index][sorted_indexes]
+    sorted_labels = labels[sorted_indexes]
+    gini_values_each_split = np.array(ginis(sorted_labels))
+    means = np.lib.stride_tricks.as_strided(sorted_column, shape=(
+        len(sorted_column)-1, 2), strides=(8, 8)).mean(axis=1)
+    return np.vstack((means, gini_values_each_split)).T
 
 
 def chi_squared_test(left_bucket, right_bucket):
@@ -72,3 +150,15 @@ def chi_squared_test(left_bucket, right_bucket):
     :param right_bucket: Bucket belonging to the right child after the split.
     :return: A float and and integer. Chi squared value and degree of freedom.
     """
+    sum_left_bucket = np.sum(left_bucket)
+    sum_right_bucket = np.sum(right_bucket)
+    rows_sum = np.array(left_bucket) + np.array(right_bucket)
+    total_sum = sum_left_bucket + sum_right_bucket
+
+    expected = np.vstack((sum_left_bucket/total_sum * np.array(rows_sum),
+    sum_right_bucket/total_sum * np.array(rows_sum)))
+    
+    actual = np.vstack((np.array(left_bucket), np.array(right_bucket)))
+    df = (np.count_nonzero(rows_sum) - 1) * (2 - 1)
+
+    return np.sum(np.nan_to_num(np.divide(np.square(actual-expected),expected))), df
